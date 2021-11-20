121fdaff666f968c4108847a7d1a5ac18d026a61
terentyevaolga <terentyeevaolga@gmail.com>
2021-11-20 23:49:06 +0300

    not norm

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -27,24 +27,41 @@ export function SignInForm() {
         }
     }
 
-    const handlerEmail = (event: any) => {
+    const validateEmail = (event: any) => {
         const email = event.target.value
+        const input = event.target
         setEmail(email)
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!re.test(String(email).toLowerCase())) {
             setInputError('Неверный логин или пароль')
+            input.classList.add('error')
+        } else {
+            input.classList.remove('error')
         }
     }
 
-    const handlerPassword = (event: any) => {
+    const validatePassword = (event: any) => {
         const password = event.target.value
+        const input = event.target
         setPassword(password)
-        if (password.length < 3 || password.length > 10) {
+
+       if (password.length < 3 || password.length > 10) {
             setInputError('Неверный логин или пароль')
+           input.classList.add('error')
         } else {
+            input.classList.remove('error')
             setInputError('')
         }
     }
+
+    // const checkButton = (event: any) => {
+    //     const input = event.target
+    //     if (inputError) {
+    //         input.classList.add('block')
+    //     } else {
+    //         input.classList.remove('block')
+    //     }
+    // }
 
     return (
             <div className="signIn">
@@ -54,7 +71,7 @@ export function SignInForm() {
                         type="text"
                         placeholder="Адрес электронной почты"
                         value={email}
-                        onChange={handlerEmail}
+                        onChange={validateEmail}
                         onBlur={handlerBlur}
                     />
                     <input
@@ -62,10 +79,10 @@ export function SignInForm() {
                         type="password"
                         placeholder="Пароль"
                         value={password}
-                        onChange={handlerPassword}
+                        onChange={validatePassword}
                         onBlur={handlerBlur}
                     />
-                    {((emailDirty || passwordDirty) && inputError) && <div style={{color: 'red', marginTop: 18}}>{inputError}</div>}
+                    {((emailDirty && passwordDirty) && inputError) && <div style={{color: 'red', marginTop: 18}}>{inputError}</div>}
                     <button disabled={!formValid} type="submit">Войти</button>
                 </form>
             </div>
